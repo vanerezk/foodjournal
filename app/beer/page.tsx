f6a9617f4c5822d2518c2e139b30beeeae2519d1
unknown <rezkvane@gmail.com>
2025-12-21 04:13:59 +0100

    beer journal2

--- app/beer/page.tsx
+++ app/beer/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import Link from "next/link";
-import { supabase } from "../lib/supabaseClient";
+import { supabase } from "../../lib/supabaseClient";
 
 // Simple beer entry type
 type BeerEntry = {
